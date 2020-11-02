6c3f2440f58b3a04b0712dd0e1e930279a99cf84
Mousa <70424449+Mousaabdi@users.noreply.github.com>
2020-11-02 14:02:15 -0800

    Update Project one Mousa.R

--- Project one Mousa.R
+++ Project one Mousa.R
@@ -1,3 +1,12 @@
+library(ggplot2)
+library(reshape2)
+data<-read in data
+data.melted<-melt(data)
+colnames(data.melted)<-rename columns 
+ggplot(data.melted,aes(x=col,y=col,color=sex)+ geom_line()
+
+
+
 
 Data <- read.csv("nc-est2015-agesex-res.csv", header=TRUE)
 Data
@@ -10,14 +19,17 @@ install.packages("growthrates")
 library(dplyr)
 #j <- table1_selected %>% rowwise() %>% mutate(Avg=mean(c(3,4), na.rm=T)) 
 #j
-men<- Data[Data$SEX==0,]
-women<-Data[Data$SEX==1,]
+women<- Data[Data$SEX==2,]
+men<-Data[Data$SEX==1,]
 men$growth_rate <- men$"2010"/men$"2015"**(1/4)-1
 women$growth_rate <- women$"2010"/women$"2015"**(1/4)-1
 colnames(men) [colnames(men)== "2015"] <-"MEN"
 colnames(women) [colnames(women)== "2015"] <-"WOMEN"
 
-men_selected <- men[ , c("AGE","MEN","growth_rate")]
-women_selected <- women[ , c("AGE","WOMEN","growth_rate")]
+M1 <- men[ , c("AGE","MEN","growth_rate")]
+W1 <- women[ , c("AGE","WOMEN","growth_rate")]
 
-#x <- cbind.data.frame(men_selected,c("WOMEN"))
+total <- merge(M1,W1,by = "AGE")
+df = subset(total, select = -c(growth_rate.y) )
+
+ggplot(data = df, aes(x = AGE, y = growth_rate.x)) +  geom_line()
